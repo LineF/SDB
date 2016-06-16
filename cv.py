b0ebc35bbc12c734e7e28556b7bc76be261d1165
Martin Patzel <martin@patzel.de>
2016-06-16 15:01:15 +0200

    open and read file

--- cv.py
+++ cv.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python
 
-import sys, getopt
+import sys, argparse, io
 
+def cvt(ifile, ofile):
+    file = open(ifile, "r", encoding="utf-8")
+    print (file.read())
+    file.close()
+
+    
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
-   print ('Input file is "', inputfile)
-   print ('Output file is "', outputfile)
+    parser = argparse.ArgumentParser(description='XML converter')
+    parser.add_argument('-i', '--input', required=True)
+    parser.add_argument('-o', '--output')
+    parser.add_argument('-v', dest='verbose', action='store_true')
+    args = parser.parse_args()
+
+    ifile = args.input
+    ofile = args.output
+    if ofile == None :
+        ofile = ifile + ".out"
+        
+    print ('Input file is "', ifile, '"', sep="")
+    print ('Output file is "', ofile, '"', sep="")
+
+    cvt(ifile, ofile)
+    
 
 if __name__ == "__main__":
    main(sys.argv[1:])
